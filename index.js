302a89c5cbddc97920d90402197d21567f6207bf
jwhit <28734055+jwhit-commit@users.noreply.github.com>
2023-10-17 13:55:00 -0500

    Improved prompt UX, added validation to text input

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const generateLogo = require('./lib/prompts.js');
 
-console.log('Welcome to the Logo Generator!\n');
+console.log('\nWelcome to the Logo Generator!\n');
 console.log('Please provide logo specs:');
 generateLogo()
